318bbd0f51af6126682699d0f470e805bdead3cd
ra2357074 <ra2357074@LDI308>
2024-04-29 09:24:29 -0300

    Add project files.

---
+++ CaixaEletronicoConsoleApp/ContaCorrente.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronicoConsoleApp
+{
+    public class CCorrente
+    {
+        public string numero;
+        public double limite;
+        public bool status;
+        public bool especial;
+        public double saldo;
+        public List<Transacao> transacoes;
+
+        //Construtores
+        public CCorrente(string numero, double limite) : this()
+        {
+            this.numero = numero;
+            this.limite = limite;
+        }
+        public CCorrente()
+        {
+            this.saldo = 0.0;
+            this.status = true;
+            transacoes = new List<Transacao>();
+        }
+
+        public bool Depositar(double valor)
+        {
+            transacoes.Add(new Transacao(valor, 'D'));
+            if (valor > 0)
+            {
+                saldo += valor;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Sacar(double valor)
+        {
+            transacoes.Add(new Transacao(valor, 'S'));
+            if (saldo - valor > -limite)
+            {
+                saldo -= valor;
+                return true;
+            }
+                return false;
+        }
+
+        public bool Transferir(CCorrente destino, double valor)
+        {
+            if(destino.status && Sacar(valor) && destino.Depositar(valor)) {
+                transacoes[transacoes.Count - 1].duplicata = destino.transacoes[destino.transacoes.Count - 1];
+                destino.transacoes[destino.transacoes.Count-1].duplicata = transacoes[transacoes.Count-1];
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
---
+++ CaixaEletronicoConsoleApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using CaixaEletronicoConsoleApp;
+
+List<CCorrente> ccorrentes = new List<CCorrente>();
+int op;
+int op2;
+int op3;
+string? s;
+do
+{
+    Console.WriteLine("==Menu==");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("1 - Acesso Administrativo");
+    Console.WriteLine("2 - Caixa Eletronico");
+    Console.WriteLine("Opcao: ");
+    s = Console.ReadLine();
+    int.TryParse(s, out op );
+
+    if (op == 1)
+    {
+        do
+        {
+            Console.WriteLine("\n\nMenu 2");
+            Console.WriteLine("0 - Voltar");
+            Console.WriteLine("1 - Cadastro da conta corrente");
+            Console.WriteLine("2 - Excluir conta corrente");
+            Console.WriteLine("Opcao: ");
+            s = Console.ReadLine();
+            int.TryParse(s, out op2 );
+
+            if (op2 == 1)
+            {
+
+            }
+            if (op2 == 2)
+            {
+
+            }
+        }while(op2 != 0);
+    }
+    if (op == 2)
+    {
+        Console.WriteLine("Qual conta voce deseja acessar?");
+
+        do
+        {
+            Console.WriteLine("\n\nMenu 3");
+            Console.WriteLine("0 - Voltar");
+            Console.WriteLine("1 - Saque");
+            Console.WriteLine("2 - Deposito");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("Opcao: ");
+            s = Console.ReadLine();
+            int.TryParse(s, out op3);
+
+            if (op3 == 1)
+            {
+
+            }
+            if (op3 == 2)
+            {
+
+            }
+            if (op3 == 3)
+            {
+
+            }
+        }while(op3 != 0);
+        
+    }
+
+} while (op != 0);
